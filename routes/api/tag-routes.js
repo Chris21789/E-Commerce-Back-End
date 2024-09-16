e0670361bdc74b619888f68a9d087303750da2f6
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-16 04:05:36 -0400

    removed a couple unused lines within the models. created the product routes. fixed an error with tag route.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     // find all tags
     const tagData = await Tag.findAll({
       // be sure to include its associated Product data
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,11 +21,11 @@ router.get('/:id', async (req, res) => {
     // find a single tag by its `id`
     const tagData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Product data
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag }]
     });
 
-    if(!tagData) {
-      res.status(404).json({ message: 'No tag found with this ID. '});
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this ID. ' });
       return;
     }
 
@@ -54,7 +54,7 @@ router.put('/:id', async (req, res) => {
       },
       individualHooks: true
     });
-    if(!tagData[0]) {
+    if (!tagData[0]) {
       res.status(404).json({ message: 'No tag found with this ID!' });
       return;
     }
