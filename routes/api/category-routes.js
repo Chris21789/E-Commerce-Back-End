7ca2efe2d5dd049102b183d0b92140840d8acd4a
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-16 03:18:21 -0400

    created the tag routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,31 +49,31 @@ router.put('/:id', async (req, res) => {
   try {
     // update a category by its `id` value
     const categoryData = await Category.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-        individualHooks: true
-      });
-      if(!categoryData[0]) {
-        res.status(404).json({ message: 'No category found with this ID!' });
-        return;
-      }
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
+      where: {
+        id: req.params.id,
+      },
+      individualHooks: true
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category found with this ID!' });
+      return;
     }
-  });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   try {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
     const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
     });
 
-    if(!categoryData) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No cateogry found with this ID!' });
       return;
     }
