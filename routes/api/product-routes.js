4f70096452a8080743e84e5e4d6653280f07a02d
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-16 04:41:46 -0400

    added an error message to a funtion in the product routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,6 +25,12 @@ router.get('/:id', async (req, res) => {
       // be sure to include its associated Category and Tag data
       include: [{ model: Category }, { model: Tag, through: ProductTag }]
     });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this ID! "});
+      return;
+    }
+    
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
